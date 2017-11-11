19d5574d1bf88f376015d3e79f141284bd3b5adb
maciej <maciekbaranowski@onet.pl>
2017-11-11 18:07:59 +0100

    Tailored bootstrap style is now part of the project - no external dependencies

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ import App from "./src/App";
 import React from "react";
 import ReactDOM from "react-dom";
 import style from "./assets/style.css";
+import bootstrap from "./assets/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("application"));
