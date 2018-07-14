dae9d2b89d39d561307a55363e3c797a8dac8fae
Maciej Baranowski <maciekbaranowski@onet.pl>
2018-07-15 01:51:51 +0200

    Minor adjustments

--- src/CV.js
+++ src/CV.js
@@ -5,12 +5,12 @@ export const Cv = () => {
   let sections = DataProvider.getCv().map((section, i) => {
     let elements = section.descriptions.map(
       (desc,i) => {
-        return <div>
+        return <div className="cv-element" key={i}>
           <grey>{desc[0]}</grey> | {desc[1]}<br/>
-          {desc[2]}<br/>
+          {desc[2]}{desc[2] === "" ? "" : <br/>}
         </div>
       });
-    return <div key={i}>
+    return <div key={i} className="cv-section">
       <h4>{section.text}</h4>
       {elements}
     </div>
--- src/Contact.js
+++ src/Contact.js
@@ -5,11 +5,7 @@ export const Contact = () => (
   <div>
     <h2>{DataProvider.getTranslations().contact}</h2>
     <span>
-      🌐 <a href="http://www.maciej-baranowski.pl">www.maciej-baranowski.pl</a>
-    </span>
-    <br />
-    <span>
-      📧 <a href="mailto:bergsoft@maciej-baranowski.pl">bergsoft@maciej-baranowski.pl</a>
+      📧 bergsoft[at]maciej-baranowski.pl
     </span>
   </div>
 );
