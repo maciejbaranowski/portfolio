f7706c8d6e1ba278b23e529e0e4dcf8c8ee9d154
Maciej Baranowski <maciekbaranowski@onet.pl>
2018-08-01 10:20:49 +0200

    Added svg icons for contact from SuperTinyIcons

--- src/Contact.js
+++ src/Contact.js
@@ -1,16 +1,21 @@
 import React from "react";
 import DataProvider from "./DataProvider";
 
+import iconEmail from "../assets/icon-email.svg";
+import iconFacebook from "../assets/icon-facebook.svg";
+import iconGithub from "../assets/icon-github.svg";
+import iconLinkedin from "../assets/icon-linkedin.svg";
+import iconStackoverflow from "../assets/icon-stackoverflow.svg";
+
 export const Contact = () => (
   <div>
     <h2>{DataProvider.getTranslations().contact}</h2>
     <div className="contact-items">
-      <span className="mini-panel">📧 E-mail: bergsoft[at]maciej-baranowski.pl</span>
-      <span className="mini-panel">LinkedIn: <a href="https://www.linkedin.com/in/maciej-baranowski-a7918779">Link</a> </span>
-      <span className="mini-panel">Facebook: <a href="https://www.facebook.com/maciej.baranowski.37">Link</a> </span>
-      <span className="mini-panel">GitHub: <a href="https://github.com/maciejbaranowski">Link</a> </span>
-      
-      <span className="mini-panel">StackOverflow:<br/>
+      <span className="mini-panel"><img src={iconEmail} className="icon"/> E-mail: bergsoft[at]maciej-baranowski.pl</span>
+      <span className="mini-panel"><img src={iconLinkedin} className="icon"/> LinkedIn: <a href="https://www.linkedin.com/in/maciej-baranowski-a7918779">Link</a> </span>
+      <span className="mini-panel"><img src={iconFacebook} className="icon"/> Facebook: <a href="https://www.facebook.com/maciej.baranowski.37">Link</a> </span>
+      <span className="mini-panel"><img src={iconGithub} className="icon"/> GitHub: <a href="https://github.com/maciejbaranowski">Link</a> </span>
+      <span className="mini-panel"><img src={iconStackoverflow} className="icon"/> StackOverflow:<br/>
     <a href="http://stackoverflow.com/users/1846995/maciej-baranowski">
       <img src="http://stackoverflow.com/users/flair/1846995.png?theme=clean"/>
     </a></span>
