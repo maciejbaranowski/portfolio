cf9b3e43be9071dd4b5fcd7238f534f327ea718d
Maciej Baranowski <maciekbaranowski@onet.pl>
2024-07-31 12:48:25 +0200

    Various old changes

--- src/Content.js
+++ src/Content.js
@@ -5,6 +5,10 @@ import {Contact} from "./Contact";
 import {Cv} from "./CV";
 import {Private} from "./Private";
 
+import "../styles/Content.css"
+
+import { CSSTransitionGroup } from 'react-transition-group';
+
 export const NONE = 0;
 export const ABOUT = 1;
 export const PROJECTS = 2;
@@ -12,19 +16,27 @@ export const CV = 3;
 export const CONTACT = 4;
 export const PRIVATE = 5;
 
-export const Content = props => {
+export const getContentPage = props => {
   switch (props.activePage) {
     case ABOUT:
-      return <About/>;
+      return <About key={1}/>;
     case PROJECTS:
-      return <Projects/>;
+      return <Projects key={2}/>;
     case CV:
-      return <Cv/>;
+      return <Cv key={3}/>;
     case CONTACT:
-      return <Contact/>;
+      return <Contact key={4}/>;
     case PRIVATE:
-      return <Private/>;
+      return <Private key={5}/>;
     default:
       return <div>Page not found</div>;
   }
 };
+
+export const Content = props => 
+  <CSSTransitionGroup 
+    transitionName="content-page"
+    transitionEnterTimeout={3000}
+    transitionLeaveTimeout={3000}>
+    {getContentPage(props)}
+  </CSSTransitionGroup>
